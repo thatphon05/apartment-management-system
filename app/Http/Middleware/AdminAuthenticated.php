e2fbd9995afd3f1fc24e9a04e3023ac9cd3e9406
thatphon05 <thatp.bas00@gmail.com>
2023-01-14 16:47:13 +0700

    Add authentication for admin and user additional (#4)

---
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthenticated
+{
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector|mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::guard('admin')->user()) {
+            return $next($request);
+        }
+        if ($request->ajax() || $request->wantsJson()) {
+            return response('Unauthorized.', 401);
+        } else {
+            return redirect(route('admin.login'));
+        }
+    }
+}
