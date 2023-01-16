97d89f188d6edc2b11d8b72f083b1655abfaeb7d
Thatpon <thatp.bas00@gmail.com>
2023-01-16 20:00:45 +0700

    Add method for get label for enum Rename In_ACTIVE to INACTIVE

--- app/Enums/AdminStatusEnum.php
+++ app/Enums/AdminStatusEnum.php
@@ -4,6 +4,26 @@ namespace App\Enums;
 
 enum AdminStatusEnum: int
 {
-    case IN_ACTIVE = 0;
+    case INACTIVE = 0;
     case ACTIVE = 1;
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public static function getLabel($value): string
+    {
+        return match ($value) {
+            'INACTIVE', 0 => 'ระงับ',
+            'ACTIVE', 1 => 'ใช้งานได้',
+        };
+    }
+
+    /**
+     * @return array
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'name', 'value');
+    }
 }
--- app/Enums/BookingStatusEnum.php
+++ app/Enums/BookingStatusEnum.php
@@ -4,6 +4,26 @@ namespace App\Enums;
 
 enum BookingStatusEnum: int
 {
-    case IN_ACTIVE = 0;
+    case INACTIVE = 0;
     case ACTIVE = 1;
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public static function getLabel($value): string
+    {
+        return match ($value) {
+            'INACTIVE', 0 => 'ระงับ',
+            'ACTIVE', 1 => 'ใช้งาน',
+        };
+    }
+
+    /**
+     * @return array
+     */
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'name', 'value');
+    }
 }
