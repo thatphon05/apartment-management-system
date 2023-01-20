c7a2e55c1174717d067beca5338a40bd79c1d492
Thatpon <thatp.bas00@gmail.com>
2023-01-20 16:57:52 +0700

    Add count query time

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,21 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
+        // Prevent Lazy loading
         Model::preventLazyLoading(!$this->app->isProduction());
+
+        // For check query number
+        if (!$this->app->isProduction()) {
+            $counter = 0;
+            $debugQuery = '';
+            DB::listen(function ($query) use (&$counter, &$debugQuery) {
+                $counter++; // increment for each query was run
+                $debugQuery = $query;
+            });
+            view()->composer('*', function ($view) use (&$counter, &$debugQuery) {
+                $view->with('countQuery', $counter);
+                $view->with('debugQuery', $debugQuery);
+            });
+        }
     }
 }
