3797e37903f0d878c8242b043a4f91f03863ca9d
Thatpon <thatp.bas00@gmail.com>
2023-01-27 13:58:58 +0700

    Add user login and dashboard

---
+++ resources/views/auths/user.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta16
+* @link https://tabler.io
+* Copyright 2018-2022 The Tabler Authors
+* Copyright 2018-2022 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>เข้าสู่ระบบ - {{ config('app.name') }}</title>
+
+    @include('partials.users.header_assets')
+
+</head>
+<body class=" border-top-wide border-primary d-flex flex-column">
+<script src="{{ asset('/dist/js/demo-theme.min.js?1668287865')  }}"></script>
+<div class="page page-center">
+    <div class="container container-tight py-4">
+        <div class="text-center mb-4">
+            <a href="." class="navbar-brand navbar-brand-autodark"><img src="{{ asset('/static/logo.svg') }}"
+                                                                        height="36" alt=""></a>
+        </div>
+        <div class="card card-md">
+            <div class="card-body">
+                <h2 class="h2 text-center mb-4">เข้าสู่ระบบผู้เช่า</h2>
+
+                @if ($errors->any())
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <div class="d-flex">
+                            <div>
+                                <!-- Download SVG icon from http://tabler-icons.io/i/alert-circle -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24"
+                                     viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                     stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <circle cx="12" cy="12" r="9"/>
+                                    <line x1="12" y1="8" x2="12" y2="12"/>
+                                    <line x1="12" y1="16" x2="12.01" y2="16"/>
+                                </svg>
+                            </div>
+                            <div>
+                                <h4 class="alert-title">เกิดข้อผิดพลาด&hellip;</h4>
+                                <div class="text-muted">
+                                    <ul>
+                                        @foreach($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+                    </div>
+                @endif
+
+                <form action="{{ route('user.login.post') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">อีเมล</label>
+                        <input type="email" name="email" class="form-control" placeholder="your@email.com"
+                               value="{{ old('email') }}" required>
+                    </div>
+                    <div class="mb-2">
+                        <label class="form-label">
+                            รหัสผ่าน
+                        </label>
+                        <div class="input-group input-group-flat">
+                            <input type="password" name="password" class="form-control" placeholder="รหัสผ่าน"
+                                   required>
+                        </div>
+                    </div>
+                    <div class="form-footer">
+                        <button type="submit" class="btn btn-primary w-100">เข้าสู่ระบบ</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('partials.users.body_assets')
+
+</body>
+</html>
---
+++ resources/views/partials/users/body_assets.blade.php
@@ -0,0 +1,8 @@
+<!-- Libs JS -->
+<script src="{{ asset('/dist/libs/apexcharts/dist/apexcharts.min.js?1668287865') }}" defer></script>
+<script src="{{ asset('/dist/libs/jsvectormap/dist/js/jsvectormap.min.js?1668287865') }}" defer></script>
+<script src="{{ asset('/dist/libs/jsvectormap/dist/maps/world.js?1668287865') }}" defer></script>
+<script src="{{ asset('/dist/libs/jsvectormap/dist/maps/world-merc.js?1668287865') }}" defer></script>
+<!-- Tabler Core -->
+<script src="{{ asset('/dist/js/tabler.min.js?1668287865') }}" defer></script>
+<script src="{{ asset('/dist/js/demo.min.js?1668287865') }}" defer></script>
