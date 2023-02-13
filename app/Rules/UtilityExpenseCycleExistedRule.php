a1c002192d5a98a341e00acf318c4b0240983b0b
Thatpon <thatp.bas00@gmail.com>
2023-02-13 08:44:39 +0700

    Use DataAwareRule

--- app/Rules/UtilityExpenseCycleExistedRule.php
+++ app/Rules/UtilityExpenseCycleExistedRule.php
@@ -5,10 +5,31 @@ namespace App\Rules;
 
 use App\Models\UtilityExpense;
 use Carbon\Carbon;
+use Illuminate\Contracts\Validation\DataAwareRule;
 use Illuminate\Contracts\Validation\InvokableRule;
 
-class UtilityExpenseCycleExistedRule implements InvokableRule
+class UtilityExpenseCycleExistedRule implements DataAwareRule, InvokableRule
 {
+    /**
+     * All of the data under validation.
+     *
+     * @var array
+     */
+    protected array $data = [];
+
+    /**
+     * Set the data under validation.
+     *
+     * @param array $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
     /**
      * Run the validation rule.
      *
@@ -21,12 +42,11 @@ class UtilityExpenseCycleExistedRule implements InvokableRule
     {
         $cycle = Carbon::parse($value);
 
-        $expenses = UtilityExpense::where('room_id', request()->room_id)
+        $expenses = UtilityExpense::where('room_id', $this->data['room_id'])
             ->whereYear('cycle', $cycle->year)
             ->whereMonth('cycle', $cycle->month)
             ->latest('id')
             ->first(['id']);
-        //->first('id');
 
         if ($expenses) {
             $fail('มีรายการมิเตอร์ของเดือนที่เลือกแล้ว');
