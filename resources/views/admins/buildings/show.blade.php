1e0d6ccc6750bda6b4f820673971b067c7a611c1
Thatpon <thatp.bas00@gmail.com>
2023-02-14 09:57:33 +0700

    Add view expense on room list

--- resources/views/admins/buildings/show.blade.php
+++ resources/views/admins/buildings/show.blade.php
@@ -78,20 +78,34 @@
                                             </td>
                                             <td>
                                                 <div class="text-end">
-                                                    <a href="{{ route('admin.rooms.show', ['room' => $room->id]) }}"
-                                                       class="btn btn-info">
-                                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                                             class="icon icon-tabler icon-tabler-eye" width="24"
-                                                             height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                             stroke="currentColor" fill="none" stroke-linecap="round"
-                                                             stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                            <circle cx="12" cy="12" r="2"></circle>
-                                                            <path
-                                                                d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
-                                                        </svg>
-                                                        ดู
-                                                    </a>
+                                                    <div class="dropdown d-inline">
+                                                        <button type="button" class="btn dropdown-toggle btn-info"
+                                                                data-bs-toggle="dropdown">
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                 class="icon icon-tabler icon-tabler-eye" width="24"
+                                                                 height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                                 stroke="currentColor" fill="none"
+                                                                 stroke-linecap="round"
+                                                                 stroke-linejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z"
+                                                                      fill="none"></path>
+                                                                <circle cx="12" cy="12" r="2"></circle>
+                                                                <path
+                                                                    d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
+                                                            </svg>
+                                                            ดู
+                                                        </button>
+                                                        <div class="dropdown-menu">
+                                                            <a class="dropdown-item"
+                                                               href="{{ route('admin.rooms.show', ['room' => $room->id]) }}">
+                                                                ดูข้อมูลห้องพัก
+                                                            </a>
+                                                            <a class="dropdown-item"
+                                                               href="{{ route('admin.expenses.show', ['roomId' => $room->id]) }}">
+                                                                ดูค่าน้ำค่าไฟ
+                                                            </a>
+                                                        </div>
+                                                    </div>
                                                     <a href="{{ route('admin.rooms.edit', ['room' => $room->id]) }}"
                                                        class="btn">
                                                         <svg xmlns="http://www.w3.org/2000/svg"
