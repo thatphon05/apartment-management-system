7f5b76c7d74c52d8b1411c7955d3eb182903401e
Thatpon <thatp.bas00@gmail.com>
2023-02-16 12:20:48 +0700

    Fix download file error

--- app/Http/Middleware/AdminAuthenticated.php
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -7,11 +7,12 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class AdminAuthenticated
 {
 
-    public function handle(Request $request, Closure $next): RedirectResponse|Response
+    public function handle(Request $request, Closure $next): RedirectResponse|Response|StreamedResponse
     {
         if (Auth::guard('admin')->user()) {
             return $next($request);
