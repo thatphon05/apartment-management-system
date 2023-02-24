bb4087dec41772fe7a228efdbab9a4b5916b0a51
Thatpon <thatp.bas00@gmail.com>
2023-02-24 14:32:02 +0700

    Prepare for repair

---
+++ resources/views/users/repairs/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.user')
+@section('title', 'รายการแจ้งซ่อม')
+@section('breadcrumb', Breadcrumbs::render('user.repair'))
+@section('content')
+    <div class="page-header d-print-none" xmlns="http://www.w3.org/1999/html">
+        <div class="container-xl">
+            <div class="row g-2 align-items-center">
+                <div class="col">
+                    <h2 class="page-title">
+                        รายการแจ้งซ่อม
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page body -->
+    <div class="page-body">
+        <div class="container-xl">
+            <h1>ว่างเปล่า</h1>
+        </div>
+    </div>
+@endsection
