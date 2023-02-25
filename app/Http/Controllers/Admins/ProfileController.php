4ab2661366d0364dd21d3e4a08acf6fcca946b9b
TanawitDew <tanawit.w@ku.th>
2023-02-25 10:32:40 +0700

    Add change password

--- app/Http/Controllers/Admins/ProfileController.php
+++ app/Http/Controllers/Admins/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admins;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ChangePasswordRequest;
+use App\Models\Admin;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
@@ -17,7 +18,23 @@ class ProfileController extends Controller
 
     public function postChangePassword(ChangePasswordRequest $request): RedirectResponse
     {
-        return to_route();
+        Admin::where('id', 1)->update(['password' => $request->password_new]);
+//        if (!(Hash::check($request->get('password_old'), auth()->admins()->password))) {
+//            // The passwords matches
+//            return redirect()->back()->with("error", "Your current password does not matches with the password.");
+//        }
+//
+//        if (strcmp($request->get('password_old'), $request->get('password_new')) == 0) {
+//            // Current password and new password same
+//            return redirect()->back()->with("error", "New Password cannot be same as your current password.");
+//        }
+//
+//        if (strcmp($request->get('password_new'), $request->get('password_new_confirmation')) == 0) {
+//            // Current password and new password same
+//            return redirect()->back()->with("error", "New Password cannot be same as your current password.");
+//        }
+
+        return redirect()->back()->with(['success' => 'เปลี่ยนรหัสผ่านสำเร็จ']);
     }
 
 }
--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -10,15 +10,18 @@ class ChangePasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'password' => 'required',
-            'password_confirmation' => 'required',
+            'password_old' => 'required',
+            'password_new' => 'required',
+            'password_new_confirmation' => 'required',
         ];
     }
 
     public function attributes(): array
     {
         return [
-
+            'password_old' => 'รหัสผ่านเก่า',
+            'password_new' => 'รหัสผ่านใหม่',
+            'password_new_confirmation' => 'ยืนยันรหัสผ่านใหม่',
         ];
     }
 
