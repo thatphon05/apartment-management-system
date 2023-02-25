b3b4757482826a782163e7310cb7f6780352fb07
TanawitDew <tanawit.w@ku.th>
2023-02-25 11:20:43 +0700

    Edit fix password

--- app/Http/Controllers/Admins/ProfileController.php
+++ app/Http/Controllers/Admins/ProfileController.php
@@ -18,21 +18,8 @@ class ProfileController extends Controller
 
     public function postChangePassword(ChangePasswordRequest $request): RedirectResponse
     {
-        Admin::where('id', 1)->update(['password' => $request->password_new]);
-//        if (!(Hash::check($request->get('password_old'), auth()->admins()->password))) {
-//            // The passwords matches
-//            return redirect()->back()->with("error", "Your current password does not matches with the password.");
-//        }
-//
-//        if (strcmp($request->get('password_old'), $request->get('password_new')) == 0) {
-//            // Current password and new password same
-//            return redirect()->back()->with("error", "New Password cannot be same as your current password.");
-//        }
-//
-//        if (strcmp($request->get('password_new'), $request->get('password_new_confirmation')) == 0) {
-//            // Current password and new password same
-//            return redirect()->back()->with("error", "New Password cannot be same as your current password.");
-//        }
+        Admin::find(auth()->guard('admin')->user()->id)
+            ->update(['password' => $request->password_new]);
 
         return redirect()->back()->with(['success' => 'เปลี่ยนรหัสผ่านสำเร็จ']);
     }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -25,6 +25,7 @@ class Admin extends Authenticatable
      */
     protected $fillable = [
         'remember_token',
+        'password',
     ];
 
     /**
